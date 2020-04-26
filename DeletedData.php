e0ce3ae1aea1e3ecc8f9164089aa2fc02ba40766
Tran Quang Duy <62538774+duytran167@users.noreply.github.com>
2020-04-26 23:41:37 +0700

    Update DeletedData.php

--- DeletedData.php
+++ DeletedData.php
@@ -1,2 +1,104 @@
-<h1>
-</h1>
+*{ margin: 0; padding: 0;
+    font-family: century gothic;
+    }
+    header{
+        background-color: #fff;
+        background-position: center;
+        height: auto;
+        background-size: cover;
+        color: black;   
+    }
+         ul
+        { 
+            float: right;
+    list-style-type: none;
+    margin-top: 7px;
+    margin-left: 17em;
+
+        }
+         ul li
+        {
+            display: inline-table;
+            width: 120px;
+            height: 40px;
+            line-height: 40px;
+            opacity: 0.8px;
+            color: black;
+            position: relative;
+            margin-top: 5px;
+            margin-bottom: -1rem;
+            
+            
+        }
+         ul li a
+        {
+                text-decoration: none;
+                color: black;
+                padding: 5px 20px;
+                border: 1px solid black
+                background-color:#dee2e663;
+                transition: 0.6s ease;
+                
+        
+        }
+        ul li a:hover {
+        
+            background-color: #f1f1f1;
+            color: #000;
+        }
+         ul li >.main {
+            display: none;
+            position: absolute;
+        }
+        ul li:hover >.main {
+            display: block;
+        }
+        ul li.active a
+        {
+            background-color: #c7ccd1;
+            color: black;
+        }
+        .logo img{
+            float: left;
+            width: 160px;
+            height: auto;
+            margin-left: 2em;
+        }
+        ul li a img{
+            margin-left: -2px;
+            margin-top: 0.6em;  
+            width: 20%;
+            float: left;
+}
+#card2{margin-top: 6em;
+    width: auto;
+    padding: 0;
+    padding-left: 6em;}
+#large{
+    height: 22em;
+    width: 100%;
+}
+#item2 {
+    display: -webkit-inline-box;
+    float: left;
+    width: 15em;
+    height: 17em;
+}
+#item2 img{
+    height: 9em;
+    width: 9em;
+    padding-top: 6em;
+}
+#item2 h3 {
+    font-size: 12px;
+    font-family: "Arial", sans-serif;
+
+}
+#item2 h5 {
+    font-size: 15px;
+    font-family: initial;
+
+}
+        </style>
+    
+</html>
